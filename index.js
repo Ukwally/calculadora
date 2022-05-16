b1890c1ba11ac96c53115a329adfa3d5a9e30ab0
ukwally <ukwally22@gmail.com>
2022-05-16 11:17:42 +0100

    fazendo upload da calculadora

---
+++ index.js
@@ -0,0 +1,32 @@
+function somar(){
+    var numero1= document.getElementById('numero1');
+    var numero2= document.getElementById('numero2');
+    var resultado = parseFloat(numero1.value) + parseFloat(numero2.value);
+} 
+function subtrair(){
+    var numero1= document.getElementById('numero1');
+    var numero2= document.getElementById('numero2');
+    var resultado = parseFloat(numero1.value) - parseFloat(numero2.value);
+    alert('o resultado é: ' + resultado );
+} 
+function dividir(){
+    var numero1= document.getElementById('numero1');
+    var numero2= document.getElementById('numero2');
+    var resultado = parseFloat(numero1.value) / parseFloat(numero2.value);
+    alert('o resultado é: ' + resultado );
+} 
+
+function multiplicar(){
+    var numero1= document.getElementById('numero1');
+    var numero2= document.getElementById('numero2');
+    var resultado = parseFloat(numero1.value) * parseFloat(numero2.value);
+    alert('o resultado é: ' + resultado );
+} 
+function limpar(){
+    var numero1= document.getElementById('numero1');
+    var numero2= document.getElementById('numero2');
+    var resultado = parseFloat(numero1.value) * parseFloat(numero2.value);
+    alert('o resultado é: ' + resultado );
+} 
+
+
