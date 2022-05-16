a51e8a6d93a22607ab6df74c4e290a98c9e19f67
ukwally <ukwally22@gmail.com>
2022-05-16 11:34:36 +0100

    correção peeração mais

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function somar(){
     var numero1= document.getElementById('numero1');
     var numero2= document.getElementById('numero2');
     var resultado = parseFloat(numero1.value) + parseFloat(numero2.value);
-} 
+    alert('o resultado é: ' + resultado );} 
 function subtrair(){
     var numero1= document.getElementById('numero1');
     var numero2= document.getElementById('numero2');
